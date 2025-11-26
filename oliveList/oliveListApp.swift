638b4a95b72d6bae9aa7925165e5746a06397f07
Hyeok-gu <thanatos6737@naver.com>
2025-11-26 12:47:36 +0900

    Initial Commit

---
+++ oliveList/oliveListApp.swift
@@ -0,0 +1,17 @@
+//
+//  oliveListApp.swift
+//  oliveList
+//
+//  Created by Daehyeok MacBook on 11/26/25.
+//
+
+import SwiftUI
+
+@main
+struct oliveListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ oliveListTests/oliveListTests.swift
@@ -0,0 +1,17 @@
+//
+//  oliveListTests.swift
+//  oliveListTests
+//
+//  Created by Daehyeok MacBook on 11/26/25.
+//
+
+import Testing
+@testable import oliveList
+
+struct oliveListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
